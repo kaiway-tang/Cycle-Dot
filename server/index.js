489d48d574b0bc138e1ebc59daa6a295b172fd56
kaiway-tang <kaiway@g.ucla.edu>
2024-04-20 20:32:00 -0700

    fix10

--- server/index.js
+++ server/index.js
@@ -1,63 +1,44 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express');        
+const cors = require('cors');                    
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = process.env.PORT || 5000; // Or any other port you prefer
 
-const mongoConnectionString = 'mongodb+srv://Kaiway:R3quirement$@cycledot.rihksa7.mongodb.net/?retryWrites=true&w=majority&appName=CycleDot';
+const mongoConnectionString = 'mongodb+srv://Kaiway:R3quirement$@cycledot.rihksa7.mongodb.net/?retryWrites=true&w=majority&appName=CycleDot'; 
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
-// Define your routes here
-// Route to save location
-app.post('/api/savelocation', (req, res) => {
-  MongoClient.connect(mongoConnectionString)
-    .then(client => {
-      const db = client.db('location_data');
-      const locationsCollection = db.collection('locations');
+let locationsCollection = {};
+app.get('/api', (req, res) => {
+  res.send("WORKING");
+});
+
+    // Route to save location
+    app.post('/api/savelocation', (req, res) => {
       locationsCollection.insertOne(req.body)
         .then(result => res.send(result))
-        .catch(error => {
-          console.error(error);
-          res.status(500).send('Error saving location');
-        })
-        .finally(() => client.close());
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send('Error connecting to database');
+        .catch(error => console.error(error));
     });
-});
 
-// Route to get all locations
-app.get('/api/locations', (req, res) => {
-  MongoClient.connect(mongoConnectionString)
-    .then(client => {
-      const db = client.db('location_data');
-      const locationsCollection = db.collection('locations');
+    app.get('/api/locations', (req, res) => {
       locationsCollection.find().toArray()
         .then(results => res.send(results))
-        .catch(error => {
-          console.error(error);
-          res.status(500).send('Error fetching locations');
-        })
-        .finally(() => client.close());
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send('Error connecting to database');
+        .catch(error => console.error(error));
     });
-});
 
-// Connect to MongoDB and start server
+// Connect to MongoDB
 MongoClient.connect(mongoConnectionString)
-  .then(() => {
+  .then(client => {
     console.log('Connected to MongoDB');
+    const db = client.db('location_data');
+    locationsCollection = db.collection('locations');
+
     app.listen(port, () => console.log(`Server listening on port ${port}`));
   })
   .catch(error => console.error(error));
 
 module.exports = app;
+
